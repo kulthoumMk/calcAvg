571c148a4683f29c802a93a1f47dbf5b5378c8f9
mustafa_kulthoum <mustafa.2420282@std.stardomuniversity.edu.eu>
2025-05-21 21:50:34 +0200

    create repo

---
+++ CalcAvgOfStudent/Form1.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnProcess = New Button()
+        btnClear = New Button()
+        btnEnd = New Button()
+        txtFirstName = New TextBox()
+        txtMidName = New TextBox()
+        txtLastName = New TextBox()
+        txtFullName = New TextBox()
+        txtAverage = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        SuspendLayout()
+        ' 
+        ' btnProcess
+        ' 
+        btnProcess.BackColor = SystemColors.HotTrack
+        btnProcess.FlatStyle = FlatStyle.Flat
+        btnProcess.Font = New Font("Times New Roman", 12F, FontStyle.Bold)
+        btnProcess.ForeColor = SystemColors.ButtonHighlight
+        btnProcess.Location = New Point(68, 384)
+        btnProcess.Name = "btnProcess"
+        btnProcess.Size = New Size(112, 46)
+        btnProcess.TabIndex = 0
+        btnProcess.Text = "Process"
+        btnProcess.UseVisualStyleBackColor = False
+        ' 
+        ' btnClear
+        ' 
+        btnClear.BackColor = SystemColors.HotTrack
+        btnClear.FlatStyle = FlatStyle.Flat
+        btnClear.Font = New Font("Times New Roman", 12F, FontStyle.Bold)
+        btnClear.ForeColor = SystemColors.ButtonHighlight
+        btnClear.Location = New Point(201, 384)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(112, 46)
+        btnClear.TabIndex = 1
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = False
+        ' 
+        ' btnEnd
+        ' 
+        btnEnd.BackColor = SystemColors.HotTrack
+        btnEnd.FlatStyle = FlatStyle.Flat
+        btnEnd.Font = New Font("Times New Roman", 12F, FontStyle.Bold)
+        btnEnd.ForeColor = SystemColors.ButtonHighlight
+        btnEnd.Location = New Point(338, 384)
+        btnEnd.Name = "btnEnd"
+        btnEnd.Size = New Size(127, 46)
+        btnEnd.TabIndex = 2
+        btnEnd.Text = "End"
+        btnEnd.UseVisualStyleBackColor = False
+        ' 
+        ' txtFirstName
+        ' 
+        txtFirstName.Font = New Font("Times New Roman", 10F, FontStyle.Bold Or FontStyle.Italic)
+        txtFirstName.Location = New Point(178, 69)
+        txtFirstName.Name = "txtFirstName"
+        txtFirstName.Size = New Size(297, 30)
+        txtFirstName.TabIndex = 0
+        ' 
+        ' txtMidName
+        ' 
+        txtMidName.Font = New Font("Times New Roman", 10F, FontStyle.Bold Or FontStyle.Italic)
+        txtMidName.Location = New Point(178, 129)
+        txtMidName.Name = "txtMidName"
+        txtMidName.Size = New Size(297, 30)
+        txtMidName.TabIndex = 4
+        ' 
+        ' txtLastName
+        ' 
+        txtLastName.Font = New Font("Times New Roman", 10F, FontStyle.Bold Or FontStyle.Italic)
+        txtLastName.Location = New Point(178, 184)
+        txtLastName.Name = "txtLastName"
+        txtLastName.Size = New Size(297, 30)
+        txtLastName.TabIndex = 5
+        ' 
+        ' txtFullName
+        ' 
+        txtFullName.Font = New Font("Times New Roman", 10F, FontStyle.Bold Or FontStyle.Italic)
+        txtFullName.Location = New Point(179, 247)
+        txtFullName.Name = "txtFullName"
+        txtFullName.Size = New Size(297, 30)
+        txtFullName.TabIndex = 6
+        ' 
+        ' txtAverage
+        ' 
+        txtAverage.Font = New Font("Times New Roman", 10F, FontStyle.Bold Or FontStyle.Italic)
+        txtAverage.Location = New Point(178, 303)
+        txtAverage.Name = "txtAverage"
+        txtAverage.Size = New Size(297, 30)
+        txtAverage.TabIndex = 7
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Bell MT", 12F, FontStyle.Bold Or FontStyle.Italic)
+        Label1.ForeColor = Color.Red
+        Label1.Location = New Point(18, 71)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(132, 29)
+        Label1.TabIndex = 8
+        Label1.Text = "First Name"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Bell MT", 12F, FontStyle.Bold Or FontStyle.Italic)
+        Label2.ForeColor = Color.Red
+        Label2.Location = New Point(19, 131)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(131, 29)
+        Label2.TabIndex = 9
+        Label2.Text = "Mide name"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Bell MT", 12F, FontStyle.Bold Or FontStyle.Italic)
+        Label3.ForeColor = Color.Red
+        Label3.Location = New Point(18, 182)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(127, 29)
+        Label3.TabIndex = 10
+        Label3.Text = "Last Name"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Bell MT", 12F, FontStyle.Bold Or FontStyle.Italic)
+        Label4.ForeColor = Color.Red
+        Label4.Location = New Point(19, 247)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(123, 29)
+        Label4.TabIndex = 11
+        Label4.Text = "Full Name"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Bell MT", 12F, FontStyle.Bold Or FontStyle.Italic)
+        Label5.ForeColor = Color.Red
+        Label5.Location = New Point(19, 303)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(102, 29)
+        Label5.TabIndex = 12
+        Label5.Text = "Average"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(9F, 26F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ControlLight
+        ClientSize = New Size(720, 468)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(txtAverage)
+        Controls.Add(txtFullName)
+        Controls.Add(txtLastName)
+        Controls.Add(txtMidName)
+        Controls.Add(txtFirstName)
+        Controls.Add(btnEnd)
+        Controls.Add(btnClear)
+        Controls.Add(btnProcess)
+        Font = New Font("Andalus", 8F)
+        ForeColor = Color.Brown
+        Name = "Form1"
+        Text = "Calculate_Avg_Student"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnProcess As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnEnd As Button
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents txtMidName As TextBox
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents txtFullName As TextBox
+    Friend WithEvents txtAverage As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+
+End Class
---
+++ CalcAvgOfStudent/Form1.vb
@@ -0,0 +1,188 @@
+﻿Public Class Form1
+    Dim r
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        r = MsgBox("Would You Like to Continue?", 4 Or 32, "Start Application")
+        If r = 7 Then
+            MessageBox.Show("Application closed.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Me.Close()
+        Else
+            txtFirstName.Focus()
+        End If
+
+        ' Set FullName and Average fields as read-only
+        txtFullName.ReadOnly = True
+        txtAverage.ReadOnly = True
+    End Sub
+
+    Private Sub btnProcess_Click(sender As Object, e As EventArgs) Handles btnProcess.Click
+        ' Check for empty name fields
+        If String.IsNullOrWhiteSpace(txtFirstName.Text) OrElse String.IsNullOrWhiteSpace(txtMidName.Text) OrElse String.IsNullOrWhiteSpace(txtLastName.Text) Then
+            MessageBox.Show("Please fill in all name fields.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        ' Check if any of the name fields contain invalid characters
+        If Not IsValidName(txtFirstName.Text) OrElse Not IsValidName(txtMidName.Text) OrElse Not IsValidName(txtLastName.Text) Then
+            MessageBox.Show("Please fill in all name fields with valid alphabetical characters only.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        ' Concatenate full name and display it
+        txtFullName.Text = $"{txtFirstName.Text} {txtMidName.Text} {txtLastName.Text}"
+
+        ' Initialize array for marks and variables for total and average
+        Dim marks(4) As Double
+        Dim total As Double = 0
+
+        ' Input marks using input boxes and calculate total
+        For i As Integer = 0 To 4
+            Dim validInput As Boolean = False
+            Dim input As String
+
+            Do
+                input = InputBox($"Enter mark {i + 1} (0-100):", "Input Marks")
+
+                ' Check if the user clicked "Cancel" or closed the InputBox
+                If String.IsNullOrWhiteSpace(input) Then
+                    MessageBox.Show("Please enter a valid mark between 0 and 100.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                    ' Validate mark input
+                ElseIf Double.TryParse(input, marks(i)) AndAlso marks(i) >= 0 AndAlso marks(i) <= 100 Then
+                validInput = True ' Mark input as valid if it's numeric and within range
+                Else
+                    MessageBox.Show("Please enter a valid mark between 0 and 100.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                End If
+            Loop Until validInput ' Repeat until valid input is entered
+
+            total += marks(i)
+        Next
+
+        ' Calculate average
+        Dim avg As Double = total / marks.Length
+        txtAverage.Text = avg.ToString("F2")
+
+        ' Show student information in a message box
+        MessageBox.Show($"Full Name: {txtFullName.Text}{Environment.NewLine}Marks: {String.Join(", ", marks)}{Environment.NewLine}Average: {avg:F2}", "Student Information", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+        ' Prompt to continue or end the program
+        Dim result As DialogResult = MessageBox.Show("Do you want to continue?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If result = DialogResult.No Then
+            Me.Close()
+        Else
+            clearAll()
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Clear all text boxes
+        clearAll()
+    End Sub
+
+    Private Sub btnEnd_Click(sender As Object, e As EventArgs) Handles btnEnd.Click
+        ' Confirm before exiting
+        Dim result As DialogResult = MessageBox.Show("Are you sure you want to exit?", "Exit Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If result = DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+    ' Function to validate name input (no leading spaces, only alphabets, max length 30)
+    Private Function IsValidName(name As String) As Boolean
+        If String.IsNullOrWhiteSpace(name) OrElse Not name.All(Function(c) Char.IsLetter(c)) OrElse name.Length > 30 Then
+            Return False
+        End If
+        Return True
+    End Function
+
+    ' Update full name automatically as the user types in name fields
+    Private Sub UpdateFullName()
+        If IsValidName(txtFirstName.Text) AndAlso IsValidName(txtMidName.Text) AndAlso IsValidName(txtLastName.Text) Then
+            txtFullName.Text = $"{txtFirstName.Text} {txtMidName.Text} {txtLastName.Text}"
+        Else
+            txtFullName.Clear()
+        End If
+    End Sub
+
+    ' Event handlers for text change to validate length and capitalize first letter
+    Private Sub txtFirstName_TextChanged(sender As Object, e As EventArgs) Handles txtFirstName.TextChanged
+        CapitalizeFirstLetter(txtFirstName)
+        If txtFirstName.Text.Length > 15 Then
+            MessageBox.Show("First name cannot exceed 15 characters.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtFirstName.Text = txtFirstName.Text.Substring(0, 15) ' Trim the text to 15 characters
+        End If
+
+        ' Check for spaces
+        If txtFirstName.Text.Contains(" ") Then
+            MessageBox.Show("First name cannot contain spaces.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtFirstName.Text = txtFirstName.Text.Replace(" ", "") ' Remove spaces
+        End If
+
+        UpdateFullName()
+    End Sub
+
+    Private Sub txtMidName_TextChanged(sender As Object, e As EventArgs) Handles txtMidName.TextChanged
+        CapitalizeFirstLetter(txtMidName)
+        If txtMidName.Text.Length > 15 Then
+            MessageBox.Show("Middle name cannot exceed 15 characters.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtMidName.Text = txtMidName.Text.Substring(0, 15) ' Trim the text to 15 characters
+        End If
+
+        ' Check for spaces
+        If txtMidName.Text.Contains(" ") Then
+            MessageBox.Show("Middle name cannot contain spaces.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtMidName.Text = txtMidName.Text.Replace(" ", "") ' Remove spaces
+        End If
+
+        UpdateFullName()
+    End Sub
+
+    Private Sub txtLastName_TextChanged(sender As Object, e As EventArgs) Handles txtLastName.TextChanged
+        CapitalizeFirstLetter(txtLastName)
+        If txtLastName.Text.Length > 15 Then
+            MessageBox.Show("Last name cannot exceed 15 characters.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtLastName.Text = txtLastName.Text.Substring(0, 15) ' Trim the text to 15 characters
+        End If
+
+        ' Check for spaces
+        If txtLastName.Text.Contains(" ") Then
+            MessageBox.Show("Last name cannot contain spaces.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtLastName.Text = txtLastName.Text.Replace(" ", "") ' Remove spaces
+        End If
+
+        UpdateFullName()
+    End Sub
+
+    ' Capitalize the first letter of the given TextBox
+    Private Sub CapitalizeFirstLetter(textBox As TextBox)
+        If textBox.Text.Length > 0 Then
+            Dim firstLetter As String = textBox.Text.Substring(0, 1).ToUpper()
+            Dim restOfString As String = textBox.Text.Substring(1).ToLower()
+            textBox.Text = firstLetter & restOfString
+            textBox.SelectionStart = textBox.Text.Length ' Set the cursor to the end of the text
+        End If
+    End Sub
+
+    ' Update individual name fields automatically if FullName is changed
+    Private Sub txtFullName_TextChanged(sender As Object, e As EventArgs) Handles txtFullName.TextChanged
+        Dim parts = txtFullName.Text.Split(New Char() {" "c}, StringSplitOptions.RemoveEmptyEntries)
+        If parts.Length > 0 Then txtFirstName.Text = parts(0)
+        If parts.Length > 1 Then txtMidName.Text = parts(1)
+        If parts.Length > 2 Then txtLastName.Text = parts(2)
+        ' Clear remaining fields if more than 3 parts are present
+        If parts.Length > 3 Then
+            txtMidName.Text = String.Empty
+            txtLastName.Text = String.Empty
+        End If
+    End Sub
+
+    Private Sub txtAverage_TextChanged(sender As Object, e As EventArgs) Handles txtAverage.TextChanged
+    End Sub
+    Private Sub clearAll()
+        txtFirstName.Clear()
+        txtMidName.Clear()
+        txtLastName.Clear()
+        txtFullName.Clear()
+        txtAverage.Clear()
+        txtFirstName.Focus()
+    End Sub
+End Class
